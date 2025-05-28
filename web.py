680541c4f57851cb90bc58b8a03ed543ebf8ff29
JustinYan9211 <jus921121@gmail.com>
2025-05-28 15:18:58 +0800

    Update web.py

--- web.py
+++ web.py
@@ -1,6 +1,8 @@
 import streamlit as st
 import pandas as pd
 import matplotlib.pyplot as plt
+plt.rcParams['font.sans-serif'] = ['Heiti TC']  
+plt.rcParams['axes.unicode_minus'] = False
 from datetime import datetime
 import io
 
